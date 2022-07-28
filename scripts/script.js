655ede894a9fca9c87380a6a17b0c365c97522b9
makshorror <makshorror96@gmail.com>
2022-07-28 16:19:44 +0300

    feat: add quotes

--- scripts/script.js
+++ scripts/script.js
@@ -37,3 +37,26 @@ window.onload = function () {
         1000);
 };
 
+//Quotes
+let hints = [];
+const hint = 'scripts/hints.json';
+fetch (hint)
+    .then (resp => {
+        return resp.json ();
+    })
+    .then (data => hints = data)
+
+function randomQuote() {
+    let quotation = document.getElementById("quote");
+    let source = document.getElementById("author");
+    let random = hints[Math.floor(Math.random() * hints.length)];
+    quotation.innerText = `“${random.quote}.”`;
+    source.innerText = random.source;
+}
+
+
+window.setInterval(function () {
+    randomQuote()
+            }, 10000);
+
+
