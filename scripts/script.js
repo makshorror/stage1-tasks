3be5387264f44ecb196c7604a1ae37adc3f2fd6f
makshorror <makshorror96@gmail.com>
2022-07-28 15:13:16 +0300

    feat: add hello

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,39 @@
 //Time and Date
 window.onload = function () {
     window.setInterval(function () {
-        let date = new Date();
-        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        let day = date.getDay();
-        let month = date.getMonth();
-        let dates = date.getDate();
+            let date = new Date();
+            let greeting;
+            let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            let day = date.getDay();
+            let month = date.getMonth();
+            let dates = date.getDate();
+
+            let hello = date.getHours();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+
+            if (hello <= 11 && 6 <= hello) {
+                greeting = "Good morning";
+            } else if (hello <= 17 && 12 <= hello) {
+                greeting = "Good afternoon";
+            } else if (hello <= 23 && 18 <= hello) {
+                greeting = "Good evening";
+            } else {
+                greeting = "Good night";
+            }
 
 
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-        let seconds = date.getSeconds();
+            if (hours < 10) hours = "0" + hours;
+            if (minutes < 10) minutes = "0" + minutes;
+            if (seconds < 10) seconds = "0" + seconds;
 
-        // if (dates < 10) dates = "0" + dates;
 
-        if (hours < 10) hours = "0" + hours;
-        if (minutes < 10) minutes = "0" + minutes;
-        if (seconds < 10) seconds = "0" + seconds;
-
-        document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
-        document.getElementById("date").innerHTML = days[day] + ", "+ dates + " " + months[month];
-        },1000);
+            document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
+            document.getElementById("date").innerHTML = days[day] + ", " + months[month] + " " + dates;
+            document.getElementById("greeting").innerHTML = greeting;
+        },
+        1000);
 };
 
