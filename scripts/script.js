9a0cc8ada8fca55468102ad846dcdda95b6fe3d4
makshorror <makshorror96@gmail.com>
2022-07-28 14:24:15 +0300

    feat: add time function

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+//Time
+
+window.onload = function () {
+    window.setInterval(function () {
+        let date = new Date();
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+        if (seconds < 10) seconds = "0" + seconds;
+
+        let clock = hours + ":" + minutes + ":" + seconds;
+
+        document.getElementById("time").innerHTML = clock;
+        },1000);
+};
