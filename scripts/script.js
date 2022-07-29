a9228cc8b2680df31504b84df4085b454789b7eb
makshorror <makshorror96@gmail.com>
2022-07-29 16:16:02 +0300

    feat: add player and weather

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
+// My API = 8ef7d1eab4a8990e718ef7a13bb2682b;
+
 //Time and Date
+
 window.onload = function () {
+    const btn_ref = document.getElementById("change-quote");
     window.setInterval(function () {
             let date = new Date();
             let greeting;
@@ -34,17 +38,21 @@ window.onload = function () {
             document.getElementById("date").innerHTML = days[day] + ", " + months[month] + " " + dates;
             document.getElementById("greeting").innerHTML = greeting;
         },
-        1000);
+        0);
+    randomQuote();
+    btn_ref.addEventListener("click", randomQuote);
 };
 
 //Quotes
+
 let hints = [];
 const hint = 'scripts/hints.json';
 fetch (hint)
     .then (resp => {
         return resp.json ();
     })
-    .then (data => hints = data)
+    .then (data => hints = data);
+
 
 function randomQuote() {
     let quotation = document.getElementById("quote");
@@ -55,8 +63,66 @@ function randomQuote() {
 }
 
 
-window.setInterval(function () {
-    randomQuote()
-            }, 10000);
+//Weather
+let city = "Minsk"
+let searchInp = document.querySelector('#city')
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        let value = searchInp.value;
+        if(!value) return false;
+        city = value;
+        init()
+    }
+})
+
+let tempBlock = document.querySelector('#temp');
+let descriptionBlock = document.querySelector('#weather-description');
+let windBlock = document.querySelector('#wind');
+let humidityBlock = document.querySelector('#humidity');
+let iconBlock = document.querySelector('#icons');
+
+
+function init() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8ef7d1eab4a8990e718ef7a13bb2682b`)
+        .then(resp => {
+            return resp.json()
+        })
+        .then(data => {
+            let imgId = data['weather']['0']['icon'];
+            iconBlock.innerHTML = `<img src='https://openweathermap.org/img/wn/${imgId}.png'>`;
+            tempBlock.textContent = `${temperature()}°C`;
+            descriptionBlock.textContent = `${data['weather']['0']['description']}`;
+            windBlock.textContent = `Wind speed: ${windSpeed()} m/s`;
+            humidityBlock.textContent = `Humidity: ${data['main']['humidity']}%`
+
+            function windSpeed(){
+                let speed = data['wind']['speed'];
+                let wS = Math.floor(speed);
+                return wS;
+            }
+            function temperature() {
+                let getTemp = data.main.temp
+                let tempC = Math.floor(getTemp) - 273
+                return tempC
+            }
+        })
+        .catch(() => {
+            alert('This city not found')
+            city = 'Minsk';
+            init()
+        })
+}
+init()
+
+
+//Player
+
+const playBtn = document.querySelector("#playPlay");
+const nextPlayBtn = document.querySelector("#nextPlay");
+const prevPlayBtn = document.querySelector("#revPlay");
+
+
+
+let songIndex = 0;
 
 
