67d7bf2d842b68a3822fa0c0948bef400412954e
makshorror <makshorror96@gmail.com>
2022-08-03 22:29:53 +0300

    feat: volume control

--- scripts/player.js
+++ scripts/player.js
@@ -1,6 +1,8 @@
 const playBtn = document.querySelector("#playPlay");
 const nextPlayBtn = document.querySelector("#nextPlay");
 const prevPlayBtn = document.querySelector("#prevPlay");
+const muteBtn = document.querySelector('#mute');
+const volumeRange = document.querySelector('#volumeRange');
 let playItem = document.querySelector(".play-item")
 const track1 = document.querySelector("#track1");
 const track2 = document.querySelector("#track2");
@@ -38,19 +40,46 @@ let trackProps = {
     muted: false,
     currentTime: 0
 };
-//Audio src
-audio.src = playList[trackProps.position].src;
-
+//Audio click
+track1.addEventListener("click",() => {
+    trackProps.position = 0;
+    trackProps.onAir = true;
+    audio.src = playList[trackProps.position].src;
+    audio.play();
+    playBtn.classList.add("pause");
+    trackProps.onAir = true;
+})
+track2.addEventListener("click",() => {
+    trackProps.position = 1;
+    trackProps.onAir = true;
+    audio.src = playList[trackProps.position].src;
+    audio.play();
+    playBtn.classList.add("pause")
+})
+track3.addEventListener("click",() => {
+    trackProps.position = 2;
+    trackProps.onAir = true;
+    audio.src = playList[trackProps.position].src;
+    audio.play();
+    playBtn.classList.add("pause")
+})
+track4.addEventListener("click",() => {
+    trackProps.position = 3;
+    trackProps.onAir = true;
+    audio.src = playList[trackProps.position].src;
+    audio.play();
+    playBtn.classList.add("pause")
+})
 
 
 //Play and Pause
+audio.src = playList[trackProps.position].src;
 playBtn.addEventListener("click", playPause)
 function playPause() {
     if (trackProps.onAir === false) {
         audio.play();
         trackProps.onAir = true;
         playBtn.classList.add("pause")
-        playItem.classList.add("item-active")
     } else {
         audio.pause();
         trackProps.onAir = false;
@@ -61,14 +90,17 @@ function playPause() {
 //Next Button
 nextPlayBtn.addEventListener('click', () => {
     if (trackProps.position < 3) {
+        trackProps.onAir = true;
         trackProps.position = trackProps.position + 1;
         audio.src = playList[trackProps.position].src;
         audio.play();
         playBtn.classList.add("pause")
     } else {
         trackProps.position = 0;
+        trackProps.onAir = true;
         audio.src = playList[trackProps.position].src;
         audio.play();
+        playBtn.classList.add("pause")
     }
 })
 
@@ -78,12 +110,63 @@ prevPlayBtn.addEventListener('click', () => {
         trackProps.position = 3;
         audio.src = playList[trackProps.position].src;
         audio.play();
+        trackProps.onAir = true;
+        playBtn.classList.add("pause")
     } else {
         trackProps.position = trackProps.position - 1;
         audio.src = playList[trackProps.position].src;
         audio.play();
+        trackProps.onAir = true;
         playBtn.classList.add("pause")
     }
 })
 
+//Mute Button
+muteBtn.addEventListener("click",() =>{
+    if (trackProps.muted === false) {
+        trackProps.muted = true;
+        muteBtn.classList.remove("play-maxvolume");
+        muteBtn.classList.add("play-mute")
+        volumeRange.value = 0;
+        trackProps.volume = 0;
+        audio.volume = 0;
+    } else {
+        trackProps.muted = false;
+        muteBtn.classList.remove("play-mute")
+        muteBtn.classList.add("play-maxvolume");
+        trackProps.volume = 100;
+        volumeRange.value = 100;
+        audio.volume = 1;
+    }
+})
 
+//Volume Control
+volumeRange.oninput = function(){
+trackProps.volume = volumeRange.value / 100;
+audio.volume = trackProps.volume;
+    if (trackProps.volume === 0){
+        trackProps.muted = true
+        muteBtn.classList.remove("play-minvolume");
+        muteBtn.classList.remove("play-mediumvolume");
+        muteBtn.classList.remove("play-maxvolume");
+        muteBtn.classList.add("play-mute")
+    } else if (trackProps.volume > 0 && trackProps.volume < 0.4) {
+        trackProps.muted = false
+        muteBtn.classList.remove("play-mute")
+        muteBtn.classList.remove("play-mediumvolume")
+        muteBtn.classList.remove("play-maxvolume")
+        muteBtn.classList.add("play-minvolume");
+    } else if (trackProps.volume > 0.4 && trackProps.volume < 0.8) {
+        trackProps.muted = false
+        muteBtn.classList.remove("play-mute")
+        muteBtn.classList.remove("play-minvolume")
+        muteBtn.classList.remove("play-maxvolume")
+        muteBtn.classList.add("play-mediumvolume");
+    } else {
+        trackProps.muted = false
+        muteBtn.classList.remove("play-mute")
+        muteBtn.classList.remove("play-minvolume")
+        muteBtn.classList.remove("play-mediumvolume")
+        muteBtn.classList.add("play-maxvolume");
+    }
+}
